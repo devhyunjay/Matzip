7755e667d3d6c262c4dac6978190da6b533b5fa6
sirri1222 <hyunnji.son@gmail.com>
2025-01-04 21:42:44 +0900

    Add: 서버 초기 세팅

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'devhyun',
+      database: 'matzip-server',
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      //  개발에서만 true
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
