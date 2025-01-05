42135cca934f2458aaf57c1ae7ab28d66ae376f5
sirri1222 <hyunnji.son@gmail.com>
2025-01-05 21:36:09 +0900

    Add: post 모듈에  Post entity import

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post])],
   providers: [PostService],
 })
 export class PostModule {}
