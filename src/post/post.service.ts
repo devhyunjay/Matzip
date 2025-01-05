773816f9dec0b62b3ae244870e17074e2a05c2c5
sirri1222 <hyunnji.son@gmail.com>
2025-01-05 21:34:41 +0900

    Add:PostService 에 createPost 추가 및 getPost 임시데이터 ->  데이터로 수정

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
 export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private postRepository: Repository<Post>,
+  ) {}
   //post 조회
-  getPosts() {
-    return ['임시 게시글'];
+  async getPosts(page: number) {
+    const perPage = 10;
+    const offset = (page - 1) * perPage;
+
+    return this.postRepository
+      .createQueryBuilder('post')
+      .orderBy('post.date', 'DESC')
+      .take(perPage)
+      .skip(offset);
+  }
+
+  //post 생성
+  async createPost(createPostDto: CreatePostDto) {
+    const {
+      latitude,
+      longtitude,
+      color,
+      address,
+      title,
+      description,
+      date,
+      score,
+    } = createPostDto;
+
+    const post = this.postRepository.create({
+      latitude,
+      longtitude,
+      color,
+      address,
+      title,
+      description,
+      date,
+      score,
+    });
+
+    try {
+      //db 에 저장
+      await this.postRepository.save(post);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException(
+        '장소를 추가하던중 에러가 발생하였습니다.',
+      );
+    }
+    return post;
   }
 }
