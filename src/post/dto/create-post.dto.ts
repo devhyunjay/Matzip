2eb535501a5d44c231bcb30e203fd1f7d68430c0
sirri1222 <hyunnji.son@gmail.com>
2025-01-05 20:21:09 +0900

    Add: class-validator 적용

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,15 +1,39 @@
 //post 생성시 client 에서 받아올 데이터
 
+import {
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 import { MarkerColor } from '../marker-color.enum';
 
 export class CreatePostDto {
+  @IsNotEmpty()
   latitude: number;
+
+  @IsNotEmpty()
   longtitude: number;
+
+  @IsNotEmpty()
   color: MarkerColor;
+
+  @IsNotEmpty()
   adress: string;
+
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsDateString()
   date: Date;
+
+  @IsNumber()
   score: number;
+
+  @IsArray()
   imageUris: { uri: string }[];
 }
