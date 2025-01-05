63a32573f8afb0cf3acd26cebef9329ca4f50755
sirri1222 <hyunnji.son@gmail.com>
2025-01-05 21:32:38 +0900

    Update: PostController 에 getPosts argument 추가 및 createPost 추가

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('post')
 export class PostController {
@@ -7,13 +8,13 @@ export class PostController {
 
   //post 조회
   @Get('/posts')
-  getPosts() {
-    return this.postService.getPosts();
+  getPosts(@Query('page') page: number) {
+    return this.postService.getPosts(page);
   }
 
   //post 생성
   @Post('/posts')
-  createPost(@Body() createPostDto: CreatePostDto){
-    return this.postService.createPost()
+  createPost(@Body() createPostDto: CreatePostDto) {
+    return this.postService.createPost(createPostDto);
   }
 }
