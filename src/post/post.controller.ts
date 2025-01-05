c424c4e19702e09dfe9ab6b7e0b1e74c8a4dfe76
sirri1222 <hyunnji.son@gmail.com>
2025-01-05 22:35:08 +0900

    Update: @Controller 수정

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 
-@Controller('post')
+@Controller()
 export class PostController {
   constructor(private postService: PostService) {}
 
